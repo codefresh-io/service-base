b2ebe303fe6fcfb297c4157657bed585b1750bfb
olegs-codefresh <olegs@codefresh.io>
2017-11-28 16:20:29 +0200

    wip

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ const service = require('./infra');
 module.exports = {
   initService: (initFn) => service.init(config, initFn),
   mongoClient: require('./infra/mongo'),
-  redisClient: require('./infra/redis'),
+  redis: require('./infra/redis'),
   validation: require('./infra/validation'),
   makeEndpoint: require('./infra/express').makeEndpoint,
   encryption: require('./infra/encryption'),
-  getAuthenticatedEntity: require('@codefresh-io/http-infra').getAuthenticatedEntity
+  getAuthenticatedEntity: require('@codefresh-io/http-infra').getAuthenticatedEntity,
+  config
 };
