ef18c8bf753dccf0d11f60bfa227e734ac16b7c8
saffi <saffi.hartal@codefresh.io>
2020-08-16 20:22:42 +0300

    SAAS-8620 update mongodb to 3.6.0 + compatibility code and test.

--- infra/__tests__/helper_test.spec.js
+++ infra/__tests__/helper_test.spec.js
@@ -1,7 +1,55 @@
-const { splitUriBySlash } = require('../helper');
+const { splitUriBySlash, getDbNameFromUri } = require('../helper');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongo = require('../mongo');
 
-it('smokeTest', async () => {
-    const { uri, dbname } = splitUriBySlash('mongodb://localhost:27017/mytestingdb');
-    expect(dbname).toBe('mytestingdb');
-    expect(uri).toBe('mongodb://localhost:27017');
+describe('mongo init compatability code', () => {
+
+    it('splitUriBySlash', async () => {
+        const { prefix, dbName } = splitUriBySlash('mongodb://localhost:27017/mytestingdb?xyz');
+        expect(dbName)
+            .toBe('mytestingdb');
+        expect(prefix)
+            .toBe('mongodb://localhost:27017');
+    });
+
+
+    it('smokeTest explicit ', async () => {
+        const mongod = new MongoMemoryServer();
+        const uri = await mongod.getUri();
+        // eslint-disable-next-line no-unused-vars
+        const port = await mongod.getPort();
+        // eslint-disable-next-line no-unused-vars
+        const dbPath = await mongod.getDbPath();
+        const dbName = await mongod.getDbName();
+        await mongo.init({ mongo: { uri, dbName } });
+        const users = mongo.collection('users');
+
+        const mockUser = { _id: 'some-user-id', name: 'John' };
+        await users.insertOne(mockUser);
+
+        const insertedUser = await users.findOne({ _id: 'some-user-id' });
+        expect(insertedUser)
+            .toEqual(mockUser);
+    });
+
+    it('smokeTest2.2.33  and 3.6 implicit dbname', async () => {
+        const mongod = new MongoMemoryServer();
+        const uri = await mongod.getUri();
+        // eslint-disable-next-line no-unused-vars
+        const port = await mongod.getPort();
+        // eslint-disable-next-line no-unused-vars
+        const dbPath = await mongod.getDbPath();
+        const dbName = await mongod.getDbName();
+        expect(dbName)
+            .toEqual(getDbNameFromUri(uri));
+        await mongo.init({ mongo: { uri } });
+        const users = mongo.collection('users');
+
+        const mockUser = { _id: 'some-user-id', name: 'John' };
+        await users.insertOne(mockUser);
+
+        const insertedUser = await users.findOne({ _id: 'some-user-id' });
+        expect(insertedUser)
+            .toEqual(mockUser);
+    });
 });
